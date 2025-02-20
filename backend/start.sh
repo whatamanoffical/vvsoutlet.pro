e9d09b666a136443fcc64d2fe58d86987dd255a8
whatamanoffical <157410409+whatamanoffical@users.noreply.github.com>
2025-02-20 10:18:53 +0100

    medusa server

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Running migrations..."
 yarn medusa db:migrate
-echo "Building admin assets..."
-yarn medusa build
+echo "Building admin assets and linking them..."
+yarn medusa build && ln -s .medusa/server/public/ public
 echo "Starting Medusa server..."
-yarn medusa start
+NODE_ENV=production medusa start -H 0.0.0.0
